8c1019efd1f8c2e84f855bc20c31c362ae88d997
James Stephaniuk <James@James.hitronhub.home>
2018-04-16 20:36:53 -0700

    initial_commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/hello/<name>/')
+def hello(name):
+    return render_template(index.html, name=name)
