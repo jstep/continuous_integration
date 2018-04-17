2da91d1a10a074b09dcf2e3489113c284a881492
James Stephaniuk <James@James.hitronhub.home>
2018-04-16 21:39:26 -0700

    canada

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, Universe!</h1>'
+    return '<h1>Hello, Canada!</h1>'
 
 @app.route('/hello/<name>/')
 def hello(name):
