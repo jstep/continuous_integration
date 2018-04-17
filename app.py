a447390d6ef4227eba72da589f8fd9d73134e6dd
James Stephaniuk <James@James.hitronhub.home>
2018-04-16 22:58:23 -0700

    fixed

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, World!</h1>'
+def index():
+    return '<h1>hi there ;)</h1>'
 
+@app.route('/hello/')
 @app.route('/hello/<name>/')
-def hello(name):
-    return render_template(index.html, name=name)
+def hello(name=None):
+    return render_template('hello.html', name=name)
